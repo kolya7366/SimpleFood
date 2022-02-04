90e1038bc2cb4f5c0d1664f2a3c8f560cd90f695
kolya7366 <nikkolya7366@gmail.com>
2022-02-04 15:19:57 +0200

    add app, fix PP, add js-scroll

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,12 @@ $(function () {
     dots: true
   });
 
+  $(".header, .hero, .popular, .footer").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+         top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 750);
+      });
+
   var mixer = mixitup('.popular__gallery');
 });
